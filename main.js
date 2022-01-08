55065d910f818fccc8e6b7b9a808493e3d2b8d89
Fabianoq <freitasfabiano08icloud.com@iMac-de-Fabiano.local>
2022-01-08 18:35:31 -0300

    Ajustes adicionados a tela principal do sistema

---
+++ main.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+const links = document.querySelectorAll('nav ul li a')
+
+for (const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+}
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+    })
+}
