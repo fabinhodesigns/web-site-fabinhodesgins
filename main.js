7c5241b350d43d556076d3e461c8f4ff3967080f
fabinho070 <freitasfabiano08@gmail.com>
2022-01-09 20:08:24 -0300

    Adicionando novas alterações

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
-const links = document.querySelectorAll('nav ul li a')
 
 for (const element of toggle) {
-    element.addEventListener('click', function() {
-        nav.classList.toggle('show')
-    })
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
 }
 
+const links = document.querySelectorAll('nav ul li a')
+
 for (const link of links) {
-    link.addEventListener('click', function() {
-        nav.classList.remove('show')
-    })
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
 }
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
