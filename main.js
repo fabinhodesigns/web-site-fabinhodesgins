5a9e7c5e292c9bd14f718324a6154571520cc3bd
Fabinho070 <freitasfabiano08@gmail.com>
2022-01-15 01:46:29 -0300

    Adicionando novos ajustes

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -7,6 +8,7 @@ for (const element of toggle) {
     })
 }
 
+/* quando clicar em um item do menu, esconder o menu */
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
@@ -15,16 +17,16 @@ for (const link of links) {
     })
 }
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
     if (window.scrollY >= navHeight) {
         header.classList.add('scroll')
     } else {
         header.classList.remove('scroll')
     }
-})
+}
 
 const swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
@@ -47,6 +49,22 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `, { interval: 100 }
 )
+
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
+
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()
+})
